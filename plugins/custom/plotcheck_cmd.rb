79d0b86b1a499c0ed81010adeae2c7db32c03c86
Clockwork EJD <4930532+ClockworkEJD@users.noreply.github.com>
2020-10-11 16:50:23 -0400

    Update plotcheck_cmd.rb

--- plugins/custom/plotcheck_cmd.rb
+++ plugins/custom/plotcheck_cmd.rb
@@ -5,12 +5,12 @@ module AresMUSH
 
             def check_can_view
                 return nil if enactor.has_permission?("view_bgs")
-                return "You are not allowed to use this command."
+                return "You cannot use this command."
             end
 
             def handle 
-                scene_info = Scenes.recent_scenes.map { |s| "#{s.icdate} - #{s.title} - #{s.plot ? s.plot.title : "%xrNO PLOT LINKED%xn" }" }.join "\n" 
-                template = BorderedDisplayTemplate.new scene_info, "Scene List"
+                scene_info = Scene.all.filter {|s| s.plots.empty? && s.shared }.map {|s| "##{s.id} - #{s.title}"}.join "\n" 
+                template = BorderedDisplayTemplate.new scene_info, "Scenes Without Plots:"
                 client.emit template.render
             end
         end
