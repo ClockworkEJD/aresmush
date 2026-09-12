3cd3c721f3247b5a82bb4958c21ec22229e34ce5
Clockwork EJD <4930532+ClockworkEJD@users.noreply.github.com>
2026-09-12 19:45:52 -0400

    Update custom.rb

--- plugins/custom/custom.rb
+++ plugins/custom/custom.rb
@@ -12,10 +12,17 @@ module AresMUSH
  
     def self.get_cmd_handler(client, cmd, enactor)   
       case cmd.root
+      when "dotcount"
+        return DotCountCmd
       when "plotcheck"
         return PlotcheckCmd
       when "wordcount"
-        return WordCountCmd
+        case cmd.switch
+        when "alts"
+          return WordCountAltsCmd
+        when nil
+          return WordCountCmd
+        end
       end   
       return nil
     end
